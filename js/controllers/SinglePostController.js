882bc4106f3b04531b7256793ea275f4668090a8
Azubuike Motanya <acmotanya@gmail.com>
2015-07-30 20:48:20 -0400

    finishiing angular part of the app

--- js/controllers/SinglePostController.js
+++ js/controllers/SinglePostController.js
@@ -2,4 +2,22 @@ app.controller('SinglePostController', ['$scope','$http', '$stateParams', functi
   $http.get('data/posts.json').success(function(data) {
     $scope.post = data[$stateParams.id];
   });
+  $scope.plusOneStory = function(index) {
+    $scope.post.storyRating += 1;
+  };
+  $scope.minusOneStory = function(index) {
+    $scope.post.storyRating -= 1;
+  };
+  $scope.plusOnePro = function(index) {
+    $scope.post.proRating += 1;
+  };
+  $scope.minusOnePro = function(index) {
+    $scope.post.proRating -= 1;
+  };
+  $scope.plusOneCon = function(index) {
+    $scope.post.conRating += 1;
+  };
+  $scope.minusOneCon = function(index) {
+    $scope.post.conRating -= 1;
+  };
 }]);
